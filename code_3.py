530d4f2fec13c54922548ab4abd9667b4c511c8d
DJSmith11 <142458119+DJSmith11@users.noreply.github.com>
2023-11-09 02:23:15 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,14 @@
+
+
+    return data
+def generate_random_data():
+
+    main()
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+def main():
+
+    for item in data:
+import random
+    data = generate_random_data()
